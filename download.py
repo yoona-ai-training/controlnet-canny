a7c7dc2b2b6cf62e4cdcfafc3e785093e1f758b6
yoona-ai-training <140383574+yoona-ai-training@users.noreply.github.com>
2023-09-20 15:47:04 +0200

    Update download.py

--- download.py
+++ download.py
@@ -3,10 +3,10 @@ import torch
 
 def download_model():
     controlnet = ControlNetModel.from_pretrained("diffusers/controlnet-canny-sdxl-1.0").to("cuda:0")
-  
+    vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix").to("cuda:0")
     pipe = StableDiffusionXLControlNetPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", controlnet=controlnet, vae=vae).to("cuda:0")
 
-    vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix").to("cuda:0")
+    
 
 if __name__ == "__main__":
     download_model()
